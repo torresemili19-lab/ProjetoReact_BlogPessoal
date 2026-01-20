2df9b6a5a8a85d4436f0280145f211031e3d5f11
torresemili19-lab <torresemili19@gmail.com>
2026-01-20 14:57:26 -0300

    ajuste para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
 	return (
@@ -10,9 +12,9 @@ function App() {
 				<Navbar />
 				<div className="min-h-[80vh]">
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/home"	element={<Home />}
-						/>
+						<Route path="/" element={<Login />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/cadastro"	element={<Cadastro />}/>
 					</Routes>
 				</div>
 				<Footer />
