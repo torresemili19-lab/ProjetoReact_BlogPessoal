4b1727afe8eed5d6dd58e763422f7ed52b693ac7
torresemili19-lab <torresemili19@gmail.com>
2026-01-20 14:02:33 -0300

    criando rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,9 +14,15 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/emili-torres-dev" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/torresemili_" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/emilitorres13" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
